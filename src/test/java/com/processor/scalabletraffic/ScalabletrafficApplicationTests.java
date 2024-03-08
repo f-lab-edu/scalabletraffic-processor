562de3d3b2a1c5e4efad573ab32f4e79b73f0895
kwongyumin <rbals7398@naver.com>
2024-03-08 12:43:32 +0900

    Initialize project setup

---
+++ src/test/java/com/processor/scalabletraffic/ScalabletrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processor.scalabletraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalabletrafficApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
